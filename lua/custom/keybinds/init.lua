d6f6a1f11d30bd7e85e94dcb0646d9dd84f48cd8
Martijn Bleyen <martijn.bleyen@gmail.com>
2025-05-02 16:17:16 +0200

    chore(diffview): added keybind <leader>g

--- lua/custom/keybinds/init.lua
+++ lua/custom/keybinds/init.lua
@@ -2,4 +2,20 @@
 vim.keymap.set('n', '<leader>c', ':bd<CR>', { desc = '[C]lose tab', silent = true })
 vim.keymap.set('n', '<leader>bo', [[:%bd|e#|bd#<CR>]], { desc = '[C]lose all [O]ther buffers', silent = true })
 
+-- Diffview
+-- vim.keymap.set('n', '<PageUp>', [[:DiffviewOpen<CR>]], { desc = 'Open Diffview', silent = true })
+
+local function toggle_diffview()
+  local view = require('diffview.lib').get_current_view()
+  if view then
+    -- Diffview is open, close it
+    vim.cmd 'DiffviewClose'
+  else
+    -- Diffview is not open, open it
+    vim.cmd 'DiffviewOpen'
+  end
+end
+
+vim.keymap.set('n', '<leader>g', toggle_diffview, { desc = 'Toggle Diffview' })
+
 return {}
