5071d9a678e7e175514cb1f1048f95d8ca4072e0
Martijn Bleyen <martijn.bleyen@gmail.com>
2025-05-02 16:07:53 +0200

    chore(statusline): modified statusline a bit

--- init.lua
+++ init.lua
@@ -928,23 +928,36 @@ require('lazy').setup({
             end
 
             local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
-            local git = MiniStatusline.section_git { trunc_width = 75 }
+            local git = MiniStatusline.section_git { trunc_width = 40 }
+            local diff = MiniStatusline.section_diff { trunc_width = 75 }
             local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+            local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
             local filename = MiniStatusline.section_filename { trunc_width = 140 }
-            local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 } -- This shows [RO] and more
+            -- local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
             local location = MiniStatusline.section_location { trunc_width = 75 }
+            local search = MiniStatusline.section_searchcount { trunc_width = 75 }
 
-            -- Remove `[RO]` by stripping it out manually from fileinfo
-            -- fileinfo = fileinfo:gsub('%[RO%]%s*', '')
+            local function fileinfo_with_icon()
+              local name = vim.api.nvim_buf_get_name(0)
+              local extension = vim.fn.fnamemodify(name, ':e')
+              local filetype = vim.bo.filetype
+
+              local icon = require('nvim-web-devicons').get_icon(name, extension, { default = true })
+              local type_label = filetype ~= '' and filetype or 'no ft'
+
+              return string.format('%s %s', icon or '', type_label)
+            end
+
+            local fileinfo = fileinfo_with_icon()
 
             return MiniStatusline.combine_groups {
               { hl = mode_hl, strings = { mode } },
-              { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
               '%<', -- Mark general truncate point
               { hl = 'MiniStatuslineFilename', strings = { filename } },
               '%=', -- End left alignment
               { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-              { hl = mode_hl, strings = { location } },
+              { hl = mode_hl, strings = { search, location } },
             }
           end,
         },
