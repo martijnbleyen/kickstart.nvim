38a0ecea8cb62baa16ad0fa50641b5f5c976e2ea
Martijn Bleyen <martijn.bleyen@gmail.com>
2025-05-04 15:59:19 +0200

    chore(lazygit): change keybind to <leader>gg

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -2,7 +2,7 @@ return {
   'kdheepak/lazygit.nvim',
   cmd = 'LazyGit',
   keys = {
-    { '<leader><leader>g', '<cmd>LazyGit<cr>', desc = 'Open LazyGit' },
+    { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'Open LazyGit' },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
